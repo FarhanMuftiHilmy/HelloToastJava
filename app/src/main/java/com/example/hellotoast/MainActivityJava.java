f34b61a0a4c71f10a1511204f67890702941ea67
Farhan Mufti Hilmy <farhan@ainosi.co.id>
2023-08-08 17:06:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hellotoast/MainActivityJava.java
@@ -0,0 +1,42 @@
+package com.example.hellotoast;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hellotoast.databinding.ActivityMainBinding;
+
+public class MainActivityJava extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private int number = 0;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.txtNumber.setText(String.valueOf(number));
+
+        binding.btnCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number++;
+                binding.txtNumber.setText(String.valueOf(number));
+            }
+        });
+
+        binding.btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivityJava.this, "Count " + number, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
+
